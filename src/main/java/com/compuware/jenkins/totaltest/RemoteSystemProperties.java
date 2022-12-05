30baf511ac70296e4cf306176dd97b82604cb273
cpwr-devops <ajaykumar_asati@bmc.com>
2022-12-05 19:38:32 +0530

    SECURITY-2624:Arbitrary file read vulnerability due to empty role check

--- src/main/java/com/compuware/jenkins/totaltest/RemoteSystemProperties.java
+++ src/main/java/com/compuware/jenkins/totaltest/RemoteSystemProperties.java
@@ -22,24 +22,16 @@ import java.util.Properties;
 import org.jenkinsci.remoting.RoleChecker;
 
 import hudson.remoting.Callable;
+import jenkins.security.MasterToSlaveCallable;
 
 /**
  * Get remote system properties
  */
-public class RemoteSystemProperties implements Callable<Properties, RuntimeException>
+public class RemoteSystemProperties extends MasterToSlaveCallable<Properties, RuntimeException>
 {
 	private static final long serialVersionUID = -8859580651709239685L;
 
-	public Properties call()
-	{
+	public Properties call() {
 		return System.getProperties();
-	}
-
-	/* (non-Javadoc)
-	 * @see org.jenkinsci.remoting.RoleSensitive#checkRoles(org.jenkinsci.remoting.RoleChecker)
-	 */
-	@Override
-	public void checkRoles(RoleChecker checker) throws SecurityException
-	{
 	}
 }
