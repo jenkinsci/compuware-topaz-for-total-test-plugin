9ce24fb63fcdb94290340d2ec53f478635c416ab
cpwr-devops <ajaykumar_asati@bmc.com>
2022-12-05 13:27:42 +0530

    Security 2625 fixed

--- src/main/java/com/compuware/jenkins/totaltest/TotalTestCTRunner.java
+++ src/main/java/com/compuware/jenkins/totaltest/TotalTestCTRunner.java
@@ -323,6 +323,12 @@ public class TotalTestCTRunner
 	private Document getXaScenarioSuiteResultAsDocument(String xml) throws Exception //NOSONAR
 	{
 		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+		dbf.setFeature("http://apache.org/xml/features/disallow-doctype-decl", true);
+		dbf.setFeature("http://xml.org/sax/features/external-general-entities", false);
+		dbf.setFeature("http://xml.org/sax/features/external-parameter-entities", false);
+		dbf.setFeature("http://apache.org/xml/features/nonvalidating/load-external-dtd", false);
+		dbf.setXIncludeAware(false);
+		dbf.setExpandEntityReferences(false);
 		DocumentBuilder db = dbf.newDocumentBuilder();
 		Reader r = new StringReader(xml);
 		return db.parse(new InputSource(r));
